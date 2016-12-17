c6de35401c8df8807adfb1805f8fff01585fa974
xelomi93 <marcelo.minay@usach.cl>
2016-12-16 22:50:27 -0300

    Arreglado error en sessions controller

--- testBicitme/app/models/bicicletum.rb
+++ testBicitme/app/models/bicicletum.rb
@@ -1,6 +1,6 @@
 class Bicicletum < ActiveRecord::Base
 	belongs_to :Ciclista
 	def combined_value
-    "#{self.bic_marca}[#{self.bic_modelo}]"
+    "Marca: #{self.bic_marca} Modelo: #{self.bic_modelo} Color: #{self.bic_color_princ}"
   end
 end
